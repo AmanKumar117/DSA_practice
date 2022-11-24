6a05837726ce9d42318355062a5e2902214c773b
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2022-11-24 21:56:43 +0530

    added swapnumber.cpp

---
+++ swapnum.py
@@ -0,0 +1,10 @@
+num1 = input(' First Number: ')
+num2 = input(' Second Number: ')
+print("Value of num1 before swapping: ", num1)
+print("Value of num2 before swapping: ", num2)
+# swapping two numbers using temporary variable
+t = num1
+num1 = num2
+num2 = t
+print("Value of num1 after swapping: ", num1)
+print("Value of num2 after swapping: ", num2)
