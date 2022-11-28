d2bddbc4273ae40cd23d7fe5863233edff56cb90
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2022-11-28 21:55:21 +0530

    added sqrt.py

---
+++ sqrt.py
@@ -0,0 +1,3 @@
+var=int(input("enter the number whose sruare root is to be found= ")) # taking input of number whose square root is to be found
+sqt= var**0.5            # operation to find and store value of square root
+print("square root of the number is= ", sqt)  # printing the square root
