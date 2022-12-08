ae461e1526190035d08912aefc7df892a3d18d9f
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2022-12-08 21:52:39 +0530

    added raise_pow.py

---
+++ raise_pow.py
@@ -0,0 +1,4 @@
+n= int(input("enter the number whose power is to be raised: "))        # input from the user for number
+power= int(input("enter the power to which number is to be raised: ")) # input from the user for power
+res= n**power      # res stores the value of operation n raised to power
+print("result= ",res) # printing result
