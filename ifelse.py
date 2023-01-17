06f66f540f482cbd2002ca7679f537d4f1278a71
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-01-17 21:43:33 +0530

    added ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,16 @@
+x=int(input("enter the value of x:-"))
+if (x%2==0):
+    print("even number")
+else:
+    print("odd number")
+a=int(input("enter the value of a:-"))
+if (a%3==0):
+    print("a is divisible by 3")
+else:
+    print("number not divisible by 3")
+if (a>0):
+    print("a is positive integer")
+elif(a<0):
+    print("a is negative integer")
+else:
+    print("number is zero")
