4298fa1405b732979556e9d3bd2c6ff2eaca332d
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-02-22 21:35:55 +0530

    added break.py

---
+++ break.py
@@ -0,0 +1,18 @@
+for letter in 'break example':     
+   if letter == 'x':
+      break
+   print ('Current Letter :', letter)
+  
+var = 5
+while var > 0:              
+   print ('Current variable value :', var)
+   var = var -1
+   if var ==1 :
+      break
+
+print ("end of programme 1")
+
+for val in "gotham city":
+    if val=="c":
+        break
+    print(val)
