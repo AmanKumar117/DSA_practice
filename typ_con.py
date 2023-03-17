82f622673275e84ad1a1cb38bafc200bf6462666
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-03-17 22:00:48 +0530

    added typ_con.py

---
+++ typ_con.py
@@ -0,0 +1,34 @@
+# number to ASCII character
+a = chr(45) 
+b = chr(44) 
+print(a) 
+print(b) 
+#real to complex
+c= complex(3,5)
+print(c)
+#int to string
+d= str(b)
+print(d)
+#tuple to dictionary
+tup= (('a', 100) ,('f', 50), ('g', 25))
+e=dict(tup)
+print(e)
+#string to list
+f= 'hello'
+g=list(f)
+print(g)
+#character to integer
+m='h'
+n=ord(m)
+print(n)
+#integer to hexadecimal string and octal string
+i= hex(n)
+j= oct(n)
+print(i,"  ",j)
+#any data type to integer and float
+s='10001001'
+p= int(s,2)
+q= float(s)
+print(p," ", q)
+
+
