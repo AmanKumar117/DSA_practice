6bb4df7b413b79264fb0ee28ff9089fedf2122d6
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-04-05 22:17:45 +0530

    added test.cpp

---
+++ test.cpp
@@ -0,0 +1,12 @@
+//addition of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the value of a and b";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"the sum of a and b is"<<c;
+    return 0;
+}
