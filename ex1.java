86204923d5bf44c6abe590e3ce8372879825a045
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-07-20 15:42:38 +0530

    added ex1.java

---
+++ ex1.java
@@ -0,0 +1,9 @@
+class ex1{
+    public static void main(String[] args){
+        int count =1 ;
+        while(count<=15){
+            System.out.println(count%2 ==1 ? "***":"+++++");
+            ++count;
+        }
+    }
+}
