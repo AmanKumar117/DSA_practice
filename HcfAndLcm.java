62880fdec5525620f94326e60f1fca865edb9037
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-07-23 15:44:01 +0530

    added hcdandlcm.java

---
+++ HcfAndLcm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class HcfAndLcm{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int hcf = 1;
+        for(int i = 1; i <= Math.min(num1, num2); i++){
+            if(num1 % i == 0 && num2 % i == 0){
+                hcf = i;
+            }
+        }
+        int lcm = (num1 *num2)/ hcf;
+        System.out.println("HCF: "+hcf+ " LCM: "+lcm);
+        sc.close();
+    }
+}
