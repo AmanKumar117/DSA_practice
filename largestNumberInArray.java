252c6345f87ad0959ab3e702563dc3ad4518d45a
Aman Kumar Singh <kumar.singhaman12324@gmail.com>
2023-07-26 15:45:25 +0530

    added largestnum.java

---
+++ largestNumberInArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class largestNumberInArray {
+    public static int getLargest(int[] a, int total){  
+        int temp;  
+        for (int i = 0; i < total; i++)   {  
+                    for (int j = i + 1; j < total; j++){  
+                        if (a[i] < a[j]){  
+                            temp = a[j];  
+                            a[j] = a[i];  
+                            a[i] = temp;  
+                        }  
+                    }  
+                }  
+               return a[0];  
+        }  
+        public static void main(String args[]){  
+            int n;
+            Scanner sc=new Scanner(System.in);  
+            System.out.print("Enter the number of elements you want to store in 1st array: ");  
+            n=sc.nextInt();  
+            int[] array = new int[n];  
+            System.out.println("Enter the elements of the array: ");  
+            for(int i=0; i<n; i++){     
+            array[i]=sc.nextInt();  
+            }   
+        System.out.println("Largest number in the array is: "+getLargest(array,n));  
+        sc.close();  
+        }
+    
+}
